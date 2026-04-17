580196a5de2a9651a132a8a977f8f0468ea90727
Gowri173 <121547683+Gowri173@users.noreply.github.com>
2026-04-17 14:55:13 +0530

    ready to deploy with dynamic refresh

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -15,6 +15,7 @@ export default function Login() {
                     localStorage.setItem("token", res.data.token)
                     localStorage.setItem("role", res.data.user.role);
                     navigate("/")
+                    window.location.reload()
                 }
             })
             .catch(err => {
